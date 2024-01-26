fdfe53a7ec07a27f0b66d16cf157c34f547ddc14
Mritunjay Singh <mritunjaysingh0011@gmail.com>
2024-01-26 15:55:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import React from 'react'
+import Nav from './components/nav'
+import Hero from './components/Hero'
+
+export default function App() {
+  return (
+    <div>
+     <Nav/>
+     <Hero/>
+    </div>
+  )
+}
